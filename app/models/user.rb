8f0bbf4123b3657e1e20f91be1af7b227de0161f
Todd Soulas <todd.soulas.au@gmail.com>
2017-05-08 08:35:55 +0000

    Add secure password and attribute to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
    validates :username, presence: true,
-            uniqueness: { case_sensitive: false },
-            length: { minimum: 3, maximum: 25 }
+             uniqueness: { case_sensitive: false },
+             length: { minimum: 3, maximum: 25 }
    validates :firstname, presence: true
    validates :lastname, presence: true
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
    validates :email, presence: true,
-   uniqueness: { case_sensitive: false },
-   format: { with: VALID_EMAIL_REGEX }
+             uniqueness: { case_sensitive: false },
+             format: { with: VALID_EMAIL_REGEX }
+   has_secure_password
+   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507090042) do
+ActiveRecord::Schema.define(version: 20170508083153) do
 
   create_table "alerts", force: :cascade do |t|
     t.string   "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170507090042) do
     t.string   "lastname"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
