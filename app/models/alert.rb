05516f6eda4550d4feacd4e0c842b542ecf160b0
Todd Soulas <todd.soulas.au@gmail.com>
2017-05-08 08:15:13 +0000

    User profiles and validations

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,8 +1,8 @@
 class Alert < ActiveRecord::Base
-    validates :title, presence: true, length: (minimum: 3, maximum 140)
-    validates :issue, presence: true, length: (minimum: 3)
-    validates :action, presence: true, length: (minimum: 3)
-    validates :source, presence: true, length: (minimum: 3)
+    validates :title, presence: true, length: {minimum: 3, maximum: 140}
+    validates :issue, presence: true, length: {minimum: 3}
+    validates :action, presence: true, length: {minimum: 3}
+    validates :source, presence: true, length: {minimum: 3}
 end
 
     
