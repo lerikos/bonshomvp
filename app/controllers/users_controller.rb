90e840b54661d27d998a0f1cd1466cf53eb0e111
Todd Soulas <todd.soulas.au@gmail.com>
2017-06-01 09:05:33 +0000

    Add user signup UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
-    def new
-    
-end
+ 
+ def new
+    @user = User.new
+ end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Bonsho #{@user.firstname}"
+      redirect_to alerts_path
+    else
+      render 'new'
+    end    
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :firstname, :lastname, :email, :password)
+    end
 
 end
