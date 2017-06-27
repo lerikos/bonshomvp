4c2663ef1a4e09f70a4b70ac22997b8aeb2880b4
Khrystyna <k.kolisnyk7@gmail.com>
2017-06-27 09:59:28 +0300

    Some fixes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
- 
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -7,22 +7,26 @@ class UsersController < ApplicationController
       redirect_to alerts_path
     else
       render 'new'
-    end    
+    end
   end
 
-def edit
-  @user = User.find(params[:id])
-end
-
-def update
-  @user = User.find(params[:id])
-  if @user.update(user_params)
-    flash[:success] = "Your account was updated successfully"
-    redirect_to alerts_path
-  else
-    render 'edit'
+  def new
+    @user = User.new
   end
-end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to alerts_path
+    else
+      render 'edit'
+    end
+  end
 
   private
     def user_params
